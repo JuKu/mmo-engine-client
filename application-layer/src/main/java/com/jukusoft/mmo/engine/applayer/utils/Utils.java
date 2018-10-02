64c7d9bf90c4b7c05df9186fd5ddfd802f29efc9
juku <kuenzel.justin@t-online.de>
2018-10-02 18:07:45 +0200

    added some junit tests for class Utils.

---
+++ application-layer/src/main/java/com/jukusoft/mmo/engine/applayer/utils/Utils.java
@@ -0,0 +1,34 @@
+package com.jukusoft.mmo.engine.applayer.utils;
+
+public class Utils {
+
+    protected Utils() {
+        //
+    }
+
+    public static String getSection (final String section) {
+        if (section == null) {
+            throw new NullPointerException("section cannot be null.");
+        }
+
+        if (section.isEmpty()) {
+            throw new IllegalArgumentException("section cannot be empty.");
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("===={ " + section + " }");
+
+        while (sb.length() < 80) {
+            sb.append("-");
+        }
+
+        return sb.toString();
+    }
+
+    public static void printSection (final String section) {
+        String s = getSection(section);
+        //LocalLogger.print("\n" + s);
+    }
+
+}
---
+++ application-layer/src/test/java/com/jukusoft/mmo/engine/applayer/utils/UtilsTest.java
@@ -0,0 +1,27 @@
+package com.jukusoft.mmo.engine.applayer.utils;
+
+import org.junit.Test;
+
+public class UtilsTest {
+
+    @Test
+    public void testConstructor () {
+        new Utils();
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testPrintNullSection () {
+        Utils.printSection(null);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testPrintEmptySection () {
+        Utils.printSection("");
+    }
+
+    @Test
+    public void testPrintSection () {
+        Utils.printSection("my-section");
+    }
+
+}
