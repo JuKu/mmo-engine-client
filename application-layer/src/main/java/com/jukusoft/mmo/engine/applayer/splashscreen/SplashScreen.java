c2b82bd1c941f576d92aa8acbf6bc874dfb44827
juku <kuenzel.justin@t-online.de>
2018-10-03 21:27:13 +0200

    added splashscreen while engine is initializing.

--- application-layer/src/main/java/com/jukusoft/mmo/engine/applayer/splashscreen/SplashScreen.java
+++ application-layer/src/main/java/com/jukusoft/mmo/engine/applayer/splashscreen/SplashScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.jukusoft.mmo.engine.applayer.config.Config;
 import com.jukusoft.mmo.engine.applayer.utils.FilePath;
 
@@ -12,6 +13,18 @@ public class SplashScreen {
     protected SpriteBatch batch = null;
 
     protected Texture bgTexture = null;
+    protected Texture logoTexture = null;
+    protected Texture animationTexture = null;
+
+    protected float animX = 0;
+    protected float animY = 0;
+    protected float animWidth = 0;
+    protected float animHeight = 0;
+
+    protected TextureRegion anim = null;
+
+    protected float degree = 0;
+    protected float rotationSpeed = 10;
 
     public SplashScreen () {
         //
@@ -25,6 +38,16 @@ public class SplashScreen {
 
         //load images
         this.bgTexture = new Texture(Gdx.files.absolute(FilePath.parse(Config.get("Splashscreen", "bgImage"))));
+        this.logoTexture = new Texture(Gdx.files.absolute(FilePath.parse(Config.get("Splashscreen", "logoImage"))));
+        this.animationTexture = new Texture(Gdx.files.absolute(FilePath.parse(Config.get("Splashscreen", "animationImage"))));
+
+        this.animX = Config.getInt("Splashscreen", "animX");
+        this.animY = Config.getInt("Splashscreen", "animY");
+        this.animWidth = Config.getInt("Splashscreen", "animWidth");
+        this.animHeight = Config.getInt("Splashscreen", "animHeight");
+        this.rotationSpeed = Config.getFloat("Splashscreen", "animSpeed");
+
+        this.anim = new TextureRegion(this.animationTexture);
     }
 
     public void render () {
@@ -36,12 +59,27 @@ public class SplashScreen {
         //render background
         batch.draw(this.bgTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
+        float x = (Gdx.graphics.getWidth() - this.logoTexture.getWidth()) / 2;
+        float y = (Gdx.graphics.getHeight() - this.logoTexture.getHeight()) / 2;
+
+        //draw logo in center
+        batch.draw(this.logoTexture, x, y, this.logoTexture.getWidth(), this.logoTexture.getHeight());
+
+        this.degree = (this.degree + Gdx.graphics.getDeltaTime() * this.rotationSpeed) % 360;
+
+        //draw rotating wheel
+        batch.draw(this.anim, this.animX, this.animY, (this.animWidth / 2), (this.animHeight / 2), this.animWidth, this.animHeight, 1, 1, this.degree);
+
         batch.end();
     }
 
     public void cleanUp () {
         this.bgTexture.dispose();
         this.bgTexture = null;
+        this.logoTexture.dispose();
+        this.logoTexture = null;
+        this.animationTexture.dispose();
+        this.animationTexture = null;
     }
 
 }
