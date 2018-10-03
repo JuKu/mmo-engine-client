28b35366d763f67fef66f8864a97a59bca14789d
juku <kuenzel.justin@t-online.de>
2018-10-03 14:47:35 +0200

    changed class JavaFXUtils, removed some duplications in code.

--- application-layer/src/main/java/com/jukusoft/mmo/engine/applayer/utils/JavaFXUtils.java
+++ application-layer/src/main/java/com/jukusoft/mmo/engine/applayer/utils/JavaFXUtils.java
@@ -63,34 +63,7 @@ public class JavaFXUtils {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle(title);
         alert.setHeaderText(headerText);
-        alert.setContentText(content);
-
-        // Create expandable Exception.
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        e.printStackTrace(pw);
-        String exceptionText = sw.toString();
-
-        Label label = new Label("The exception stacktrace was:");
-
-        TextArea textArea = new TextArea(exceptionText);
-        textArea.setEditable(false);
-        textArea.setWrapText(true);
-
-        textArea.setMaxWidth(Double.MAX_VALUE);
-        textArea.setMaxHeight(Double.MAX_VALUE);
-        GridPane.setVgrow(textArea, Priority.ALWAYS);
-        GridPane.setHgrow(textArea, Priority.ALWAYS);
-
-        GridPane expContent = new GridPane();
-        expContent.setMaxWidth(Double.MAX_VALUE);
-        expContent.add(label, 0, 0);
-        expContent.add(textArea, 0, 1);
-
-        //set expandable Exception into the dialog pane.
-        alert.getDialogPane().setExpandableContent(expContent);
-
-        alert.showAndWait();
+        showExceptionDlg(alert, content, e);
     }
 
     public static void showExceptionDialog (String title, String content, Throwable e) {
@@ -99,6 +72,10 @@ public class JavaFXUtils {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle(title);
         alert.setHeaderText(null);
+        showExceptionDlg(alert, content, e);
+    }
+
+    private static void showExceptionDlg (Alert alert, String content, Throwable e) {
         alert.setContentText(content);
 
         // Create expandable Exception.
