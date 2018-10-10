86451afd3a37d384487199edd1d535657b18ebff
juku <kuenzel.justin@t-online.de>
2018-10-10 16:49:32 +0200

    changed class InputLayer.

--- game-view-layer/src/main/java/com/jukusoft/mmo/engine/gameview/InputLayer.java
+++ game-view-layer/src/main/java/com/jukusoft/mmo/engine/gameview/InputLayer.java
@@ -1,6 +1,7 @@
 package com.jukusoft.mmo.engine.gameview;
 
 import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.ControllerAdapter;
 import com.badlogic.gdx.controllers.Controllers;
 import com.jukusoft.mmo.engine.applayer.config.Config;
 import com.jukusoft.mmo.engine.applayer.logger.Log;
@@ -19,9 +20,20 @@ public class InputLayer implements SubSystem {
             for (Controller controller : Controllers.getControllers()) {
                 Log.i("Controller", "controller detected: " + controller.getName());
                 connectedControllers++;
+
+                //initialize controller
+                this.initController(controller);
             }
 
             Log.i("Controller", connectedControllers + " controller(s) detected.");
+
+            Controllers.addListener(new ControllerAdapter() {
+                @Override
+                public void connected(Controller controller) {
+                    Log.i("Controller", "new controller detected: " + controller.getName());
+                    initController(controller);
+                }
+            });
         } else {
             Log.i("Controller", "controller support is disabled.");
         }
@@ -35,6 +47,15 @@ public class InputLayer implements SubSystem {
     @Override
     public void onShutdown() {
         //
+    }
+
+    protected void initController (Controller controller) {
+        controller.addListener(new ControllerAdapter() {
+            @Override
+            public void disconnected(Controller controller) {
+                Log.i("Controller", "controller disconnected: " + controller.getName());
+            }
+        });
     }
 
 }
