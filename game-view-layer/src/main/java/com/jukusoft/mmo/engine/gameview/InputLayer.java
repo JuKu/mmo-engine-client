bd8484403465c2dd5d9cf380e39b64f562f5e641
juku <kuenzel.justin@t-online.de>
2018-10-10 16:34:15 +0200

    added support for controllers, closed #4 .

--- game-view-layer/src/main/java/com/jukusoft/mmo/engine/gameview/InputLayer.java
+++ game-view-layer/src/main/java/com/jukusoft/mmo/engine/gameview/InputLayer.java
@@ -1,5 +1,7 @@
 package com.jukusoft.mmo.engine.gameview;
 
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.Controllers;
 import com.jukusoft.mmo.engine.applayer.logger.Log;
 import com.jukusoft.mmo.engine.applayer.subsystem.SubSystem;
 
@@ -8,6 +10,16 @@ public class InputLayer implements SubSystem {
     @Override
     public void onInit() {
         Log.i("Input", "initializing input devices");
+
+        //check for connected controllers
+        int connectedControllers = 0;
+
+        for (Controller controller : Controllers.getControllers()) {
+            Log.i("Controller", "controller detected: " + controller.getName());
+            connectedControllers++;
+        }
+
+        Log.i("Controller", connectedControllers + " controller(s) detected.");
     }
 
     @Override
