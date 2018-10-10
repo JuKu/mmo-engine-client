c3b8b60eae292970a0199d0c81cfe2f1bcb2b051
juku <kuenzel.justin@t-online.de>
2018-10-10 16:41:03 +0200

    added new configuration options for controllers.

--- game-view-layer/src/main/java/com/jukusoft/mmo/engine/gameview/InputLayer.java
+++ game-view-layer/src/main/java/com/jukusoft/mmo/engine/gameview/InputLayer.java
@@ -2,6 +2,7 @@ package com.jukusoft.mmo.engine.gameview;
 
 import com.badlogic.gdx.controllers.Controller;
 import com.badlogic.gdx.controllers.Controllers;
+import com.jukusoft.mmo.engine.applayer.config.Config;
 import com.jukusoft.mmo.engine.applayer.logger.Log;
 import com.jukusoft.mmo.engine.applayer.subsystem.SubSystem;
 
@@ -11,15 +12,19 @@ public class InputLayer implements SubSystem {
     public void onInit() {
         Log.i("Input", "initializing input devices");
 
-        //check for connected controllers
-        int connectedControllers = 0;
+        if (Config.getBool("Controller", "enabled")) {
+            //check for connected controllers
+            int connectedControllers = 0;
 
-        for (Controller controller : Controllers.getControllers()) {
-            Log.i("Controller", "controller detected: " + controller.getName());
-            connectedControllers++;
+            for (Controller controller : Controllers.getControllers()) {
+                Log.i("Controller", "controller detected: " + controller.getName());
+                connectedControllers++;
+            }
+
+            Log.i("Controller", connectedControllers + " controller(s) detected.");
+        } else {
+            Log.i("Controller", "controller support is disabled.");
         }
-
-        Log.i("Controller", connectedControllers + " controller(s) detected.");
     }
 
     @Override
