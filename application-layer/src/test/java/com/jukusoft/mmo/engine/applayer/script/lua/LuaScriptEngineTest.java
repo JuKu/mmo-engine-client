f19a13acd8ec02317c1ed42076318828bf9a96df
juku <kuenzel.justin@t-online.de>
2018-10-12 01:52:01 +0200

    added some junit tests for lua script engine.

--- application-layer/src/test/java/com/jukusoft/mmo/engine/applayer/script/lua/LuaScriptEngineTest.java
+++ application-layer/src/test/java/com/jukusoft/mmo/engine/applayer/script/lua/LuaScriptEngineTest.java
@@ -1,6 +1,7 @@
 package com.jukusoft.mmo.engine.applayer.script.lua;
 
 import com.jukusoft.mmo.engine.applayer.script.exception.ScriptLoadException;
+import com.jukusoft.mmo.engine.applayer.time.GameTime;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -29,6 +30,23 @@ public class LuaScriptEngineTest {
         LuaScriptEngine engine = new LuaScriptEngine();
         engine.compile("test", "return 2");
         assertEquals(2l, engine.execFunc("test"));
+    }
+
+    @Test
+    public void testExec1 () throws ScriptLoadException {
+        LuaScriptEngine engine = new LuaScriptEngine();
+        engine.compile("test", "return 2");
+        assertEquals(2l, engine.execFunc("test", "param1", "param2"));
+    }
+
+    @Test
+    public void testExecGlobalFunc () throws ScriptLoadException {
+        GameTime time = GameTime.getInstance();
+        time.setTime(System.currentTimeMillis());
+
+        LuaScriptEngine engine = new LuaScriptEngine();
+        engine.compile("test", "return now();");
+        assertEquals(time.getTime(), engine.execFunc("test"));
     }
 
 }
