534e1a9d299a635b6cef26b5a56f73e1727080f6
juku <kuenzel.justin@t-online.de>
2018-10-19 18:44:34 +0200

    added process manager.

--- shared-library/src/main/java/com/jukusoft/mmo/engine/shared/process/ProcessManager.java
+++ shared-library/src/main/java/com/jukusoft/mmo/engine/shared/process/ProcessManager.java
@@ -2,6 +2,6 @@ package com.jukusoft.mmo.engine.shared.process;
 
 public interface ProcessManager {
 
-    //
+    public void update ();
 
 }
---
+++ shared-library/src/main/java/com/jukusoft/mmo/engine/shared/process/impl/DefaultProcessManager.java
@@ -0,0 +1,12 @@
+package com.jukusoft.mmo.engine.shared.process.impl;
+
+import com.jukusoft.mmo.engine.shared.process.ProcessManager;
+
+public class DefaultProcessManager implements ProcessManager {
+
+    @Override
+    public void update() {
+        //
+    }
+
+}
