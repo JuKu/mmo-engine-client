cee92306c1e8acaff5d327c59248388103320ca2
juku <kuenzel.justin@t-online.de>
2018-11-11 00:49:02 +0100

    changed maven configuration, upgraded version 0.0.6-SNAPSHOT to 0.0.7-SNAPSHOT so proxy server can use newest version of class Config.

--- startup/start_profiling.bat
+++ startup/start_profiling.bat
@@ -1,1 +1,1 @@
-java -jar mmo-engine-client-desktop-0.0.6-SNAPSHOT.jar --permit-illegal-access -Dcom.sun.management.jmxremote
+java -jar mmo-engine-client-desktop-0.0.7-SNAPSHOT.jar --permit-illegal-access -Dcom.sun.management.jmxremote
