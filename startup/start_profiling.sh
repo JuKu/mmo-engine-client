1ac31ae39059ac14c37ed3051dad60173b2bc36e
juku <kuenzel.justin@t-online.de>
2018-11-11 16:19:17 +0100

    changed maven configuration, upgraded version from 0.0.10-SNAPSHOT to 0.0.11-SNAPSHOT.

--- startup/start_profiling.sh
+++ startup/start_profiling.sh
@@ -1,1 +1,1 @@
-java -jar mmo-engine-client-desktop-0.0.10-SNAPSHOT.jar --permit-illegal-access -Dcom.sun.management.jmxremote
+java -jar mmo-engine-client-desktop-0.0.11-SNAPSHOT.jar --permit-illegal-access -Dcom.sun.management.jmxremote
