cd1ae4381c6529743a1793113275a4037fab3fb6
juku <kuenzel.justin@t-online.de>
2018-11-15 00:36:31 +0100

    changed maven configuration, upgraded version from 0.0.13-SNAPSHOT to 0.0.14-SNAPSHOT.

--- startup/start.bat
+++ startup/start.bat
@@ -1,1 +1,1 @@
-java -jar mmo-engine-client-desktop-0.0.13-SNAPSHOT.jar --permit-illegal-access
+java -jar mmo-engine-client-desktop-0.0.14-SNAPSHOT.jar --permit-illegal-access
--- startup/start_profiling.sh
+++ startup/start_profiling.sh
@@ -1,1 +1,1 @@
-java -jar mmo-engine-client-desktop-0.0.13-SNAPSHOT.jar --permit-illegal-access -Dcom.sun.management.jmxremote
+java -jar mmo-engine-client-desktop-0.0.14-SNAPSHOT.jar --permit-illegal-access -Dcom.sun.management.jmxremote
